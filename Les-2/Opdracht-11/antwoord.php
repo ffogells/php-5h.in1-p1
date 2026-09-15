4dfd66f9947bed3c0159c044e15aae681f879d9a
ffogells <samllg+github@proton.me>
2026-09-15 09:58:43 +0200

    Aanpassing opdracht 11

--- Les-2/Opdracht-11/antwoord.php
+++ Les-2/Opdracht-11/antwoord.php
@@ -7,11 +7,11 @@
 <body>
     <?php
         $antwoord = $_POST["hoofdstad"];
-        if ($antwoord == "b") {
+        if ($antwoord == "rennes") {
             echo "<h1>Dat klopt!</h1>\n";}
         else {
             echo "<h1>Dat is fout!</h1>";
-            echo "<p>Het goede antwoord was Berlijn.</p>";}
+            echo "<p>Het goede antwoord was Rennes.</p>";}
     ?>
 </body>
 </html>
--- Les-2/Opdracht-11/hoofdstad.php
+++ Les-2/Opdracht-11/hoofdstad.php
@@ -5,12 +5,12 @@
     <title>Opdracht 11 hoofdstad</title>
 </head>
 <body>
-    <h1>Wat is de hoofdstad van Duitsland?</h1>
+    <h1>Wat is de hoofdstad van Bretagne?</h1>
     <p>
         <form method="POST" action="antwoord.php">
-            <input type="radio" name="hoofdstad" value="bn">Bonn<br>
-            <input type="radio" name="hoofdstad" value="b">Berlijn<br>
-            <input type="radio" name="hoofdstad" value="do">Dortmund<br>
+            <input type="radio" name="hoofdstad" value="brest">Brest<br>
+            <input type="radio" name="hoofdstad" value="nantes">Nantes<br>
+            <input type="radio" name="hoofdstad" value="rennes">Rennes<br>
             <input type="submit" value="Verstuur">
         </form>
     </p>
