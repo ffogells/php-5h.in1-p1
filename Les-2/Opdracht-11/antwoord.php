aa4387cd34cf70135a05e3af1a3f8b7cb34373e8
ffogells <samllg+github@proton.me>
2026-09-15 09:51:38 +0200

    Opdracht 11

---
+++ Les-2/Opdracht-11/antwoord.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Opdracht 11 resultaat</title>
+</head>
+<body>
+    <?php
+        $antwoord = $_POST["hoofdstad"];
+        if ($antwoord == "b") {
+            echo "<h1>Dat klopt!</h1>\n";}
+        else {
+            echo "<h1>Dat is fout!</h1>";
+            echo "<p>Het goede antwoord was Berlijn.</p>";} 
+    ?>
+</body>
+</html>
---
+++ Les-2/Opdracht-11/hoofdstad.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Opdracht 11 hoofdstad</title>
+</head>
+<body>
+    <h1>Wat is de hoofdstad van Duitsland?</h1>
+    <p>
+        <form method="POST" action="antwoord.php">
+            <input type="radio" name="hoofdstad" value="bn">Bonn<br>
+            <input type="radio" name="hoofdstad" value="b">Berlijn<br>
+            <input type="radio" name="hoofdstad" value="do">Dortmund<br>
+            <input type="submit" value="Verstuur">
+        </form>
+    </p>
+</body>
+</html>
