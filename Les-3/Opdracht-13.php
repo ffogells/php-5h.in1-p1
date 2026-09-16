b96d51bfabe7e4c47187e12a0b7dc254c11c8c30
ffogells <samllg+github@proton.me>
2026-09-16 10:11:40 +0200

    Opdracht 13

---
+++ Les-3/Opdracht-13.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Opdracht 13</title>
+</head>
+<body>
+    <?php
+    for($aantal = 8 ; $aantal >= 0 ; $aantal--){
+        echo $aantal."<br>\n";
+    }?>
+    <br>
+    <?php 
+    
+    
+    
+    
+    
+    
+    ?>
+</body>
+</html>
