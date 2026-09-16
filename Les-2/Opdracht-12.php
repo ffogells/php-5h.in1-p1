fae3a0f947b0df1f4df962ac2537ce26c4de77e9
ffogells <samllg+github@proton.me>
2026-09-16 09:51:59 +0200

    Opdracht 12

--- Les-2/Opdracht-12.php
+++ Les-2/Opdracht-12.php
@@ -21,19 +21,12 @@
     </p>
   </form>
   <?php
- // if (!empty($_GET["getal1"] && $_GET["getal2"] && $_GET["operatie"])) {
- //   if ($_GET["operatie"] == "+") {$antwoord = $_GET["getal1"] + $_GET["getal2"]}
- //   elseif ($_GET["operatie"] == "×") {$antwoord = $_GET["getal1"] * $_GET["getal2"]}
- //   elseif ($_GET["operatie"] == "-") {$antwoord = $_GET["getal1"] - $_GET["getal2"]}
- //   elseif ($_GET["operatie"] == "/") {$antwoord = $_GET["getal1"] / $_GET["getal2"]}
- // }?>
-
-
-// <?php
-//$antwoord = $_GET["operatie"];
-//echo "HALLO HIER STAAT HET HOPELIJK" . $antwoord;
-// ?>
-
+  if (!empty($_GET["getal1"]) && !empty ($_GET["getal2"]) && !empty ($_GET["operatie"])) {
+    if ($_GET["operatie"] == "+") {$antwoord = $_GET["getal1"] + $_GET["getal2"];}
+    elseif ($_GET["operatie"] == "×") {$antwoord = $_GET["getal1"] * $_GET["getal2"];}
+    elseif ($_GET["operatie"] == "-") {$antwoord = $_GET["getal1"] - $_GET["getal2"];}
+    elseif ($_GET["operatie"] == "/") {$antwoord = $_GET["getal1"] / $_GET["getal2"];}
+  }?>
   <p>Antwoord: <input type="text" value="<?php if(!empty($antwoord)) {echo $antwoord;} ?>"></p>
 </body>
 </html>
