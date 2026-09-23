ac8b862b6d96845b3641bfd29aa68c5ae03c5835
ffogells <samllg+github@proton.me>
2026-09-23 09:29:47 +0200

    15 geupdate om de opdracht te volgen

--- Les-3/Opdracht-15.php
+++ Les-3/Opdracht-15.php
@@ -10,18 +10,11 @@
         <select>
             <?php 
             $jaar = date("Y");
-            $jaarstop = date("Y") - 130;
-            while($jaar >= $jaarstop) {
+            while($jaar >= 2000) {
                 echo "<option>".$jaar."</option>\n";
                 $jaar--;
             }?>
             <?php
-            //De opdracht zei tot 2000, ik heb dit gedaan want dat is logischer voor een geboortejaar, maar de code voor tot 2000 zou dit zijn:
-            //
-            //$jaar = date("Y");
-            //while($jaar >= 2000) {
-            //    echo "<option>".$jaar."</option>\n";
-            //    $jaar--;
             ?>
         </select>
     </form>
